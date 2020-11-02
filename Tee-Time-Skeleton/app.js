0e1c5ae23f06019b6ddb5232849827bc4e8983b1
ethanswe <ethanaschnitzer@gmail.com>
2020-11-02 15:45:36 -0700

    adjusted app.js

--- Tee-Time-Skeleton/app.js
+++ Tee-Time-Skeleton/app.js
@@ -5,19 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize }  = require('./db/models');
 const store = new SequelizeStore({
   db: sequelize,
 });
 
 
-app.use(
-  session({
-    secret: 'superSecret',
-    store,
-    resave: false,
-  })
-);
-store.sync();
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -26,17 +20,26 @@ const signupRouter = require('./routes/signup');
 
 const app = express();
 
-app.use(signupRouter);
 
-// view engine setup
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+  session({
+    secret: 'superSecret',
+    store,
+    resave: false,
+  })
+  );
+  store.sync();
+  
+  // view engine setup
+  app.set('view engine', 'pug');
+  
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(signupRouter);
+  
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
