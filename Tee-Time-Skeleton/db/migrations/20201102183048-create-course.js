52004d585e5282eda485867ca73bce31e8d14653
ethanswe <ethanaschnitzer@gmail.com>
2020-11-02 11:43:18 -0700

    city association / course model generated

---
+++ Tee-Time-Skeleton/db/migrations/20201102183048-create-course.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        unique: true,
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      cityId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Cities' },
+      },
+      difficulty: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      numHoles: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      minPrice: {
+        allowNull: false,
+        type: Sequelize.NUMERIC(6,2)
+      },
+      maxPrice: {
+        allowNull: false,
+        type: Sequelize.NUMERIC(6,2)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Courses');
+  }
+};
