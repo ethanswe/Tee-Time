8db4b04a1f1d0055192dcab466d842e42388db2b
maxwelljmckee <maxwelljmckee@gmail.com>
2020-11-04 11:47:11 -0700

    auth 100% debugged!!

--- Tee-Time-Skeleton/public/javascripts/index.js
+++ Tee-Time-Skeleton/public/javascripts/index.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
 })
+
+window.addEventListener('DOMContentLoaded', e => {
+    document.querySelector('.logout-button')
+        .addEventListener('click', async (e) => {
+            e.preventDefault();
+            await fetch('/logout', {
+                method: 'POST',
+            })
+            window.location.href = '/';
+        })
+})
