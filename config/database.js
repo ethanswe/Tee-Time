f1c634d837660070849efc5c51f4fe26c4fc7409
ethanswe <ethanaschnitzer@gmail.com>
2020-11-05 09:57:04 -0700

    repo refactor

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'postgres://dvlzynaipmepxk:4d0926416eeb9c6e017c166e8ef878768acc32848f262f8c288ef6ca5a2a073c@ec2-52-200-134-180.compute-1.amazonaws.com:5432/dc3eq2e38fao29',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
