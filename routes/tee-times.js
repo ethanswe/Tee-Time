478d8814809f9e8ab50a1a1d3dfbe95a2010490a
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-11-05 12:47:29 -0600

    min max price search

--- routes/tee-times.js
+++ routes/tee-times.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../auth');
 const db = require('../db/models');
+const { asyncHandler } = require('./utils');
 
 // GET TEETIMES SPLASH PAGE //
 router.get('/', (req, res) => {
@@ -9,16 +10,34 @@ router.get('/', (req, res) => {
   res.render('tee-times', { title: 'TeeTimes' });
 })
 
- 
+
 // GET CREATE TEETIME FORM //
 router.get('/create', requireAuth, (req, res) => {
   res.send('Create New TeeTime Form')
 })
 
+router.get('/', asyncHandler(async(req, res) => {
+  const {  }
+  const filteredCourses = await db.Course.findAll({
+    where: {
+      minPrice: {
+        [Op.gte]: amount
+      },
+      maxPrice: {
+        [Op.lte]: amount
+      }
+    }
+  });
+    res.render('tee-times', {
+      filteredCourses,
+      csrfToken: req.csrfToken(),
+    })
+}));
+
 
 // POST NEW TEETIME //
 router.post('/', requireAuth, (req, res) => {
-  
+
 })
 
 router.delete('/:id(\\d+)', requireAuth, (req, res) => {
