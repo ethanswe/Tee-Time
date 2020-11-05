2e0378acb37146fa6f07b02b3e7dbd756c791b52
ethanswe <ethanaschnitzer@gmail.com>
2020-11-05 09:26:09 -0700

    heroku push

--- Tee-Time-Skeleton/config/database.js
+++ Tee-Time-Skeleton/config/database.js
@@ -6,12 +6,18 @@ const {
 } = require('./index').db;
 
 
+
 module.exports = {
   development: {
     username,
     password,
     database,
     host,
-    dialect: 'postgres'
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'postgres://dvlzynaipmepxk:4d0926416eeb9c6e017c166e8ef878768acc32848f262f8c288ef6ca5a2a073c@ec2-52-200-134-180.compute-1.amazonaws.com:5432/dc3eq2e38fao29',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 };
