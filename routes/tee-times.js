a95b5acdeddcd1d001aa529ab4a0af8567166fb0
maxwelljmckee <maxwelljmckee@gmail.com>
2020-11-06 09:50:56 -0700

    added a few files

--- routes/tee-times.js
+++ routes/tee-times.js
@@ -9,6 +9,7 @@ const {
   teeTimeValidators
 } = require('./utils');
 
+
 // GET TEETIMES SPLASH PAGE //
 router.get('/', asyncHandler(async (req, res) => {
   let teeTimes = await db.TeeTime.findAll({
@@ -28,7 +29,6 @@ router.get(
   '/create', 
   csrfProtection, 
   asyncHandler(async (req, res) => {
-  // const cities = await db.City.findAll({ order: ['name'] });
   const courses = await db.Course.findAll();
   const playStyles = await db.PlayStyle.findAll();
   const teeTime = {}
@@ -92,6 +92,8 @@ router.post(
     playStyleId
   })
 
+  console.log(courseId);
+  console.log(playStyleId);
   // console.log(date);
 
   const validationErrors = validationResult(req)
